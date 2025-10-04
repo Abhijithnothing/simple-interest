8630566e0a7389330c11086bcd03c79cc48f449c
Abhijith <a00931668@gmail.com>
2025-10-04 09:25:33 +0530

    Update SI.py

--- SI.py
+++ SI.py
@@ -1,6 +1,14 @@
-a=int(input("Enter priciple amt:"))
-b=int(input("Enter time period:"))
-c=int(input("Enter rate of interest:"))
-print("--- simple interest --")
-SI=(a+b+c)/100
-print("Simple interest is ",SI)
+# Simple & Compound Interest Program
+
+P = int(input("Enter principal amount: "))
+T = int(input("Enter time period (in years): "))
+R = float(input("Enter rate of interest: "))
+
+print("--- Simple Interest ---")
+SI = (P * R * T) / 100
+print("Simple Interest is:", SI)
+
+print("--- Compound Interest ---")
+A = P * (1 + R/100) ** T
+CI = A - P
+print("Compound Interest is:", CI)
