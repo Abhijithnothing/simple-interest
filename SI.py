c4c9f42ab259d8eed737d8baac947bb8b4e67308
Abhijith <a00931668@gmail.com>
2025-10-04 09:19:30 +0530

    Create SI.py

---
+++ SI.py
@@ -0,0 +1,6 @@
+a=int(input("Enter priciple amt:"))
+b=int(input("Enter time period:"))
+c=int(input("Enter rate of interest:"))
+print("--- simple interest --")
+SI=(a+b+c)/100
+print("Simple interest is ",SI)
